e20ef28f4b13d87bd67ea82cce9c34af4dd95fcb
Jonathan Toledo <jtoledo@opalo.com.co>
2018-06-25 11:41:00 -0500

    WolframAlpha SendImage

--- plugins/wolfram.py
+++ plugins/wolfram.py
@@ -1,6 +1,8 @@
 import wolframalpha
+import re
 from settings import CREDENTIALS
 from utils import evalRegex
+from utils import downloadImage
 client = wolframalpha.Client(CREDENTIALS["wolframalpha"])
 
 def process_message(message, msg_sender, msg_to, msg_type, connector, bot):
@@ -12,10 +14,14 @@ def process_message(message, msg_sender, msg_to, msg_type, connector, bot):
         for pod in res.pods:
             if pod.primary or pod.title in ["Result"] or re.search('plot', pod.title, re.IGNORECASE):
                 for sub in pod.subpods:
+                    send_text = True
                     for img in sub.img:
                         if img.src:
-                            print("send image", img.src) #TODO: enviar imagen
-                    if sub.plaintext:
+                            file = downloadImage(img.src)
+                            if file:
+                                send_text = False
+                                connector.send_image(msg_to, file, sub.plaintext)
+                    if send_text and sub.plaintext:
                         connector.send_message(msg_to, str(sub.plaintext))
             else:
                 print(pod.title)
